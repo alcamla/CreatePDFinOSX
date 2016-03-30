b42c31b0319e772d114834489aad145bddcc83d9
Alejandro Camacho Laverde <camacholaverde@gmail.com>
2016-03-30 15:05:16 -0500

    Initial Commit

---
+++ TestingPDFCreation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestingPDFCreation
+//
+//  Created by camacholaverde on 3/30/16.
+//  Copyright © 2016 gibicgroup. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
